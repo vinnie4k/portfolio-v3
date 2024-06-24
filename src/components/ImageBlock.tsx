08a1cc895a9c3cb001061e9556c2d76e6d444a1d
Vin Bui <vdb23@cornell.edu>
2024-06-24 17:18:27 -0400

    Add image zoom to blog/work detail

--- src/components/ImageBlock.tsx
+++ src/components/ImageBlock.tsx
@@ -1,3 +1,6 @@
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+
 interface props {
   imageSrc: any;
   imageAlt: string;
@@ -13,7 +16,9 @@ export default function ImageBlock({
 }: props) {
   return (
     <div className="flex flex-col gap-3">
-      <img src={imageSrc.src} alt={imageAlt} className={imageStyle} />
+      <Zoom>
+        <img src={imageSrc.src} alt={imageAlt} className={imageStyle} />
+      </Zoom>
       {caption && (
         <p className="b2 text-neutral-600 dark:text-neutral-400 text-center">
           {caption}
